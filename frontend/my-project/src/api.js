e355c4d57a341f4118b1f876f28d22a630f3e4a7
sikindra97 <sikindramahto374@gmail.com>
2026-06-07 02:45:15 +0530

    Update api.js

--- frontend/my-project/src/api.js
+++ frontend/my-project/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-expense-tracker-179d.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
